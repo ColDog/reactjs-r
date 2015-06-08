26c86ed26414d604fcd81680c1ddac40709b9a04
Colin Walker <cjwalker@sfu.ca>
2015-06-08 10:38:06 -0700

    remove date process

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,11 +4,4 @@ class Article < ActiveRecord::Base
   validates :title, presence: true
   validates :body, presence: true
 
-  before_create :date_process
-
-  private
-    def date_process
-      self.created_at.strftime('%B %d')
-    end
-
 end
